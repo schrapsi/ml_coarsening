63ca7759284e6fd706811cc1c45e8bbec7d7d5b3
schrapsi <ucfoh@student.kit.edu>
2025-05-22 17:44:02 +0200

    Refactor dropout parameter in CustomizableDenseNet: rename dropout_rate to dropout

--- src/models/components/customizable_dense_net.py
+++ src/models/components/customizable_dense_net.py
@@ -4,15 +4,15 @@ from torch import nn
 
 class CustomizableDenseNet(nn.Module):
 
-    def __init__(self, input_size=208, hidden_sizes=[512, 256], output_size=1, dropout_rate=0.3, with_sigmoid=True):
+    def __init__(self, input_size=208, hidden_sizes=[512, 256], output_size=1, dropout=0.3, with_sigmoid=True):
         super().__init__()
         self.flatten = nn.Flatten()
-        layers = [nn.Linear(input_size, hidden_sizes[0]), nn.ReLU(), nn.Dropout(p=dropout_rate)]
+        layers = [nn.Linear(input_size, hidden_sizes[0]), nn.ReLU(), nn.Dropout(p=dropout)]
 
         for i in range(1, len(hidden_sizes)):
             layers.append(nn.Linear(hidden_sizes[i - 1], hidden_sizes[i]))
             layers.append(nn.ReLU())
-            layers.append(nn.Dropout(p=dropout_rate))
+            layers.append(nn.Dropout(p=dropout))
 
         layers.append(nn.Linear(hidden_sizes[-1], output_size))
         if with_sigmoid:
