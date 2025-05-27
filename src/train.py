83e7b59ba10ea72a88e59c7e800b372092ebc1a9
schrapsi <ucfoh@student.kit.edu>
2025-05-27 17:12:11 +0200

    Refactor default.yaml and early_stopping.yaml for improved logging and configuration; add logging utilities

--- src/train.py
+++ src/train.py
@@ -8,14 +8,17 @@ from lightning.pytorch.loggers import Logger
 from hydra.utils import to_absolute_path
 import joblib
 
-torch.set_float32_matmul_precision('medium')
+from utils.logging_utils import log_hyperparameters
+from utils.pylogger import RankedLogger
 
+torch.set_float32_matmul_precision('medium')
+log = RankedLogger(__name__, rank_zero_only=True)
 
 def instantiate_callbacks(callbacks_cfg: DictConfig) -> List[Callback]:
     callbacks: List[Callback] = []
 
     if not callbacks_cfg:
-        #log.warning("No callback configs found! Skipping..")
+        log.warning("No callback configs found! Skipping..")
         return callbacks
 
     if not isinstance(callbacks_cfg, DictConfig):
@@ -23,28 +26,53 @@ def instantiate_callbacks(callbacks_cfg: DictConfig) -> List[Callback]:
 
     for _, cb_conf in callbacks_cfg.items():
         if isinstance(cb_conf, DictConfig) and "_target_" in cb_conf:
-            #log.info(f"Instantiating callback <{cb_conf._target_}>")
+            log.info(f"Instantiating callback <{cb_conf._target_}>")
             callbacks.append(hydra.utils.instantiate(cb_conf))
 
     return callbacks
 
+def get_metric_value(metric_dict: Dict[str, Any], metric_name: Optional[str]) -> Optional[float]:
+    """Safely retrieves value of the metric logged in LightningModule.
+
+    :param metric_dict: A dict containing metric values.
+    :param metric_name: If provided, the name of the metric to retrieve.
+    :return: If a metric name was provided, the value of the metric.
+    """
+    if not metric_name:
+        log.info("Metric name is None! Skipping metric value retrieval...")
+        return None
+
+    if metric_name not in metric_dict:
+        raise Exception(
+            f"Metric value not found! <metric_name={metric_name}>\n"
+            "Make sure metric name logged in LightningModule is correct!\n"
+            "Make sure `optimized_metric` name in `hparams_search` config is correct!"
+        )
+
+    metric_value = metric_dict[metric_name].item()
+    log.info(f"Retrieved metric value! <{metric_name}={metric_value}>")
+
+    return metric_value
+
 
 def train(cfg: DictConfig) -> Tuple[Dict[str, Any], Dict[str, Any]]:
-    #log.info(f"Instantiating datamodule <{cfg.data._target_}>")
+    log.info(f"Instantiating datamodule <{cfg.data._target_}>")
     datamodule: LightningDataModule = hydra.utils.instantiate(cfg.data)
-
     cfg.model.net.input_size = datamodule.get_feature_count()
 
+    log.info(f"Instantiating model <{cfg.model._target_}>")
     model: LightningModule = hydra.utils.instantiate(cfg.model)
     model.hparams.scaler = datamodule.scaler
 
+    log.info("Instantiating callbacks...")
     callbacks: List[Callback] = instantiate_callbacks(cfg.get("callbacks"))
 
+    log.info("Instantiating loggers...")
     logger = None
     if not cfg.get("logger") is None:
         logger: Logger = hydra.utils.instantiate(cfg.logger.mlflow) or None
 
-
+    log.info(f"Instantiating trainer <{cfg.trainer._target_}>")
     trainer: Trainer = hydra.utils.instantiate(cfg.trainer, callbacks=callbacks, logger=logger)
 
     object_dict = {
@@ -52,21 +80,33 @@ def train(cfg: DictConfig) -> Tuple[Dict[str, Any], Dict[str, Any]]:
         "datamodule": datamodule,
         "model": model,
         "trainer": trainer,
+        "callbacks": callbacks,
+        "logger": logger,
     }
+    if logger:
+        log.info("Logging hyperparameters!")
+        log_hyperparameters(object_dict)
+
     if cfg.get("train"):
-        #log.info("Starting training!")
+        log.info("Starting training!")
         trainer.fit(model=model, datamodule=datamodule, ckpt_path=cfg.get("ckpt_path"))
 
     train_metrics = trainer.callback_metrics
 
+    early_stopping_callback = callbacks[1]
+    if early_stopping_callback.stopped_epoch != -1:  # -1 is the default value when not stopped
+        log.info(f"Early stopping triggered at epoch {early_stopping_callback.stopped_epoch}")
+    else:
+        log.info("Training completed without early stopping")
+
     if cfg.get("test"):
-        #log.info("Starting testing!")
+        log.info("Starting testing!")
         ckpt_path = trainer.checkpoint_callback.best_model_path
         if ckpt_path == "":
-            #log.warning("Best ckpt not found! Using current weights for testing...")
+            log.warning("Best ckpt not found! Using current weights for testing...")
             ckpt_path = None
         trainer.test(model=model, datamodule=datamodule, ckpt_path=ckpt_path)
-        #log.info(f"Best ckpt path: {ckpt_path}")
+        log.info(f"Best ckpt path: {ckpt_path}")
 
     test_metrics = trainer.callback_metrics
 
@@ -84,12 +124,12 @@ def main(cfg: DictConfig) -> Optional[float]:
     metric_dict, _ = train(cfg)
 
     # safely retrieve metric value for hydra-based hyperparameter optimization
-    #metric_value = get_metric_value(
-    #    metric_dict=metric_dict, metric_name=cfg.get("optimized_metric")
-    #)
+    metric_value = get_metric_value(
+        metric_dict=metric_dict, metric_name=cfg.get("optimized_metric")
+    )
 
     # return optimized metric
-    #return metric_value
+    return metric_value
 
 
 if __name__ == "__main__":
