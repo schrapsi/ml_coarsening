2d8d3e8456bd5966b2d682b96e0a6f27644b819f
schrapsi <ucfoh@student.kit.edu>
2025-05-28 12:40:21 +0200

    Add functionality to copy METIS files during inference and update prediction directory structure

--- src/inference.py
+++ src/inference.py
@@ -1,3 +1,5 @@
+import os
+import shutil
 from pathlib import Path
 from typing import Optional, Tuple, Dict, Any
 
@@ -11,6 +13,14 @@ from lightning.pytorch import Trainer
 
 def inference(cfg: DictConfig):
     model = MLCoarseningModule.load_from_checkpoint(cfg.ckpt_path)
+
+    ckpt_path = Path(cfg.ckpt_path)
+    run_dir = ckpt_path.parent.parent
+    model_name = run_dir.name
+    pred_dir = Path(cfg.pred_path) / model_name
+    pred_dir.mkdir(exist_ok=True, parents=True)
+
+
     scaler = model.hparams.scaler
     features = model.hparams.features
     datamodule: LightningDataModule = hydra.utils.instantiate(cfg.data, scaler=scaler, features=features)
@@ -20,13 +30,14 @@ def inference(cfg: DictConfig):
 
     dataloaders = datamodule.predict_dataloader()
     trainer: Trainer = Trainer()
+    copy_metis_files(cfg.metis_path, pred_dir, dataloaders.keys())
     for graph in dataloaders:
         dl = dataloaders[graph]
         print(f"Predict dataloader: {graph}, with {len(dl.dataset)} samples")
         raw_outputs = trainer.predict(model, dataloaders=dl)
         ids = raw_outputs[0]["ids"]  # shape [B, 2]
         preds = raw_outputs[0]["preds"]
-        write_to_file(cfg.pred_path, graph, ids, preds)
+        write_to_file(pred_dir, graph, ids, preds)
 
 
 def write_to_file(path, graph_name, ids, preds):
@@ -41,6 +52,20 @@ def write_to_file(path, graph_name, ids, preds):
             f.write(f"{int(hi)},{int(lo)},{float(p)}\n")
 
     print(f"Wrote {out_path}")
+
+
+def copy_metis_files(src_folder, dest_folder, graph_set):
+    # Iterate through files in the source folder
+    for graph in graph_set:
+        filename = graph + ".metis"
+        src_path = os.path.join(src_folder, filename)
+        dest_path = os.path.join(dest_folder, filename)
+
+        if os.path.isfile(src_path):
+            shutil.copy2(src_path, dest_path)  # copy2 preserves metadata
+            print(f"Copied: {src_path} -> {dest_path}")
+        else:
+            print(f"File not found: {src_path}, skipping copy.")
 
 
 @hydra.main(version_base="1.3", config_path="../configs", config_name="inference.yaml")
