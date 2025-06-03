8273ba10fb502b896b7e8ca519cdbefc03c5a306
simeon <ucfoh@student.kit.edu>
2025-06-03 10:39:11 +0200

    Add cost_0_half.txt for feature configuration and update training script to use new features file

--- scripts/exec_scripts/train.sh
+++ scripts/exec_scripts/train.sh
@@ -6,4 +6,4 @@ spack env activate ml_coarsening
 source .venv/bin/activate
 git pull
 #srun uv run -m src.train trainer.enable_progress_bar=False data=mss_1_20
-srun uv run -m src.train trainer.enable_progress_bar=False data=mss_1_20 model=ml_coarsening_bce
+srun uv run -m src.train trainer.enable_progress_bar=False data=mss_1_20 data.features_file=${paths.root_dir}/configs/data/features/cost_0_half.txt
