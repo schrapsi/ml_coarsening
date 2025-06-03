02a541c9d3a350b2cc3128e2600d15bb39164e34
schrapsi <ucfoh@student.kit.edu>
2025-06-03 16:11:17 +0200

    Add cost_0_3quarter.txt for feature configuration and update training script to include log directory

--- scripts/exec_scripts/train.sh
+++ scripts/exec_scripts/train.sh
@@ -6,4 +6,9 @@ spack env activate ml_coarsening
 source .venv/bin/activate
 git pull
 #srun uv run -m src.train trainer.enable_progress_bar=False data=mss_1_20
-srun uv run -m src.train trainer.enable_progress_bar=False data=mss_1_20 data.features_file=/nfs/home/schrape/ml_coarsening/configs/data/features/cost_0_half.txt
+srun uv run -m src.train \
+ trainer.enable_progress_bar=False \
+ data=mss_1_20 \
+ trainer=gpu \
+ data.features_file=/nfs/home/schrape/ml_coarsening/configs/data/features/cost_0_half.txt \
+ paths.log_dir=/nfs/work/students/ml_coarsening
