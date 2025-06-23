a112705a00c33a2b6376a3262a026e7d79a1bdae
schrapsi <ucfoh@student.kit.edu>
2025-06-23 09:40:39 +0200

    Update multi_classification.yaml and train.sh with new graph and feature file paths

--- scripts/exec_scripts/train.sh
+++ scripts/exec_scripts/train.sh
@@ -8,6 +8,7 @@ git pull
 srun uv run -m src.train \
  trainer.enable_progress_bar=False \
  trainer=gpu \
- data.features_file=/nfs/home/schrape/ml_coarsening/configs/data/features/cost_0_41.txt \
- data.graphs_file=/nfs/home/schrape/ml_coarsening/configs/data/graphs/mss_21_45.txt \
+ experiment=multi_classification \
+ data.features_file=/nfs/home/schrape/ml_coarsening/configs/data/features/all.txt \
+ data.graphs_file=/nfs/home/schrape/ml_coarsening/configs/data/graphs/mss_46_69.txt \
  paths.log_dir=/nfs/work/students/ml_coarsening/logs/
