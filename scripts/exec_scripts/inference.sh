86e163fc8f82010b3d944888eaf5416c29d0fa9f
schrapsi <ucfoh@student.kit.edu>
2025-08-16 15:44:30 +0200

    Update inference script to use dynamic paths for model checkpoints and graph files

--- scripts/exec_scripts/inference.sh
+++ scripts/exec_scripts/inference.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+#!/bin/bash
+RUNS_DIR=/nfs/work/students/ml_coarsening/logs/train/runs
+GRAPHS_DIR=$HOME/ml_coarsening/configs/data/graphs
+
+GRAPH_SET=mss_1_20_eval
+MODEL_DIR=2025-08-11_balanced_ram_761
+EPOCH=018
+MODEL_CLASS="MLCoarseningModule"
+
 cd ~ || exit
 cd ml_coarsening || exit
 spack env activate ml_coarsening
 source .venv/bin/activate
 git pull
 HYDRA_FULL_ERROR=1 srun uv run -m src.inference \
-  ckpt_path=/nfs/work/students/ml_coarsening/logs/train/runs/2025-06-03_clever_grasshopper_761/checkpoints/epoch_022.ckpt
+  ckpt_path=$RUNS_DIR/$MODEL_DIR/checkpoints/epoch_$EPOCH.ckpt \
+  data.graphs_file=$GRAPHS_DIR/$GRAPH_SET.txt \
+  model_class=$MODEL_CLASS
