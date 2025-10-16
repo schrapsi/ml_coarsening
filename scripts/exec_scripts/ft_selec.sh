68c6b998960e31f6025a376cf583c90d1c697d3d
simeon <ucfoh@student.kit.edu>
2025-10-16 15:46:03 +0200

    update feature selection and training scripts for improved data handling

--- scripts/exec_scripts/ft_selec.sh
+++ scripts/exec_scripts/ft_selec.sh
@@ -5,7 +5,7 @@ cd ml_coarsening || exit
 spack env activate ml_coarsening
 source .venv/bin/activate
 git pull
-srun uv run -m src.feature_selection \
- data.features_file=/nfs/home/schrape/ml_coarsening/configs/data/features/cost_0_reduced.txt \
+HYDRA_FULL_ERROR=1 srun uv run -m src.feature_selection \
+ data.features_file=/nfs/home/schrape/ml_coarsening/configs/data/features/cost_0_det.txt \
  data.graphs_file=/nfs/home/schrape/ml_coarsening/configs/data/graphs/mss_1_20.txt \
  top_n=32
--- scripts/exec_scripts/train.sh
+++ scripts/exec_scripts/train.sh
@@ -7,11 +7,11 @@ source .venv/bin/activate
 git pull
 srun uv run -m src.train \
  trainer.enable_progress_bar=False \
- trainer=gpu \
+ trainer=cpu \
  model.net.hidden_sizes="[48, 32, 16]" \
  model.net.with_batch_norm=False \
  model.optimizer.weight_decay=0.00001 \
- data.features_file=/nfs/home/schrape/ml_coarsening/configs/data/features/top_32_features_of_cost_0_reduced_for_mss_1_20.txt \
+ data.features_file=/nfs/home/schrape/ml_coarsening/configs/data/features/top_32_features.txt \
  data.graphs_file=/nfs/home/schrape/ml_coarsening/configs/data/graphs/mss_1_20.txt \
  data.data_amount=1000000 \
  paths.log_dir=/nfs/work/students/ml_coarsening/logs/
