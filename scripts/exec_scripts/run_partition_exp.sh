0692f989eab8f9e5d5afab8ea3c551f03c440b1c
simeon <ucfoh@student.kit.edu>
2025-11-09 11:35:32 +0100

    update inference script with new graph set and model directory configurations

--- scripts/exec_scripts/run_partition_exp.sh
+++ scripts/exec_scripts/run_partition_exp.sh
@@ -4,8 +4,8 @@ GRAPHS_DIR=$HOME/ml_coarsening/configs/data/graphs
 TODAY_DATE=$(date +"%Y-%-m-%-d")
 
 
-GRAPH_SET=mss_1_20_eval
-MODEL_DIR=2000-01-01_loving_chipmunk_in_lvl1
+GRAPH_SET=mss_1_20
+MODEL_DIR=2000-01-01_loving_chipmunk_in_2r_0.4t
 MODEL_BRANCH=cheap_model
 
 
@@ -18,7 +18,8 @@ git pull
 HYDRA_FULL_ERROR=1 srun uv run -m src.utils.direct_inference_setup \
   model_dir=$RUNS_DIR/$MODEL_DIR/ \
   data.graphs_file=$GRAPHS_DIR/$GRAPH_SET.txt \
-  'flags="--c-guided-coarsening-levels=1"'
+  'flags="--c-guided-subrounds=2 --c-guiding-threshold=0.4"'
+
 
 
 cd ~ || exit
