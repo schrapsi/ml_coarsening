2c5b851882346925c6ae214275013f862d535e8b
simeon <ucfoh@student.kit.edu>
2025-11-12 12:46:43 +0100

    script changes

--- scripts/exec_scripts/run_exp_wpred.sh
+++ scripts/exec_scripts/run_exp_wpred.sh
@@ -2,15 +2,16 @@
 RUNS_DIR=/nfs/work/students/ml_coarsening/logs/train/runs
 GRAPHS_DIR=$HOME/ml_coarsening/configs/data/graphs
 
-GRAPH_SET=mss_1_20_eval
-MODEL_DIR=2025-11-09_kind_beaver_587
-EPOCH=022
+GRAPH_SET=mss_21_45
+MODEL_DIR=2025-10-18_loving_chipmunk_692
+EPOCH=021
 MODEL_CLASS="MLCoarseningModule"
 
 cd ~ || exit
 cd ml_coarsening || exit
 spack env activate ml_coarsening
 source .venv/bin/activate
+git checkout main
 git pull
 HYDRA_FULL_ERROR=1 srun uv run -m src.inference \
   ckpt_path=$RUNS_DIR/$MODEL_DIR/checkpoints/epoch_$EPOCH.ckpt \
